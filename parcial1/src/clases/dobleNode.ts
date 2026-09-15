b7eea49c3f0e8ab9f8021cddbbe887e9027f9ce9
miderzzz <carlos.mideros@uao.edu.co>
2026-09-15 17:27:07 -0500

    challenge 1

--- parcial1/src/clases/dobleNode.ts
+++
@@ -1,9 +0,0 @@
-export class DoubleNode<T> {
-    value: T;
-    next: DoubleNode<T> | null;
-    prev: DoubleNode<T> | null;
-    constructor(value: T) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
-    }}
--- parcial1/src/clases/nodeCircular.ts
+++
@@ -1,9 +0,0 @@
-export class CircularNode<T> {
-    value: T;
-    next: CircularNode<T> | null;
-
-    constructor(value: T) {
-        this.value = value;
-        this.next = null;
-    }
-}
