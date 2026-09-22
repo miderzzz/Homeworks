7fb6fda7d6071830506d1568e1546e19d23d2d40
miderzzz <carlos.mideros@uao.edu.co>
2026-09-21 20:18:41 -0500

    Delete challenge-4 directory

--- challenge-4/src/data/librosData.js
+++
@@ -1,5 +0,0 @@
-export const booksData = [
-  { id: 1, bookName: 'Cien años de soledad', isbn: '978-0307474728', author: 'García Márquez', editorial: 'Sudamericana' },
-  { id: 2, bookName: '1984', isbn: '978-0451524935', author: 'George Orwell', editorial: 'Secker & Warburg' },
-  { id: 3, bookName: 'El principito', isbn: '978-0156012195', author: 'Antoine de Saint-Exupéry', editorial: 'Reynal & Hitchcock' },
-];
